1469900695bb1382984530e35df28150fdd2a427
semihalev <semihalev@gmail.com>
2025-05-21 16:03:42 +0300

    Added global semantic versioning

---
+++ internal/common/version.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2025 Stoolap Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+const (
+	// VersionMajor is the major version of the driver
+	VersionMajor = "0"
+	// VersionMinor is the minor version of the driver
+	VersionMinor = "0"
+	// VersionPatch is the patch version of the driver
+	VersionPatch = "1"
+	// VersionSuffix is the suffix of the driver version
+	VersionSuffix = "alpha" // or "beta", "rc", etc.
+
+	// VersionString is the version string of the driver
+	VersionString = "Stoolap v" + VersionMajor + "." + VersionMinor + "." + VersionPatch + "-" + VersionSuffix
+)
---
+++ internal/functions/scalar/version.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2025 Stoolap Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scalar
+
+import (
+	"github.com/stoolap/stoolap/internal/common"
+	"github.com/stoolap/stoolap/internal/functions/contract"
+	"github.com/stoolap/stoolap/internal/functions/registry"
+	"github.com/stoolap/stoolap/internal/parser/funcregistry"
+)
+
+// VersionFunction implements the VERSION() function
+// This function is commonly used by database tools and ORMs to check database compatibility
+type VersionFunction struct{}
+
+// Name returns the name of the function
+func (f *VersionFunction) Name() string {
+	return "VERSION"
+}
+
+// GetInfo returns the function information
+func (f *VersionFunction) GetInfo() funcregistry.FunctionInfo {
+	return funcregistry.FunctionInfo{
+		Name:        "VERSION",
+		Type:        funcregistry.ScalarFunction,
+		Description: "Returns the Stoolap database version string",
+		Signature: funcregistry.FunctionSignature{
+			ReturnType:    funcregistry.TypeString,
+			ArgumentTypes: []funcregistry.DataType{},
+			MinArgs:       0,
+			MaxArgs:       0,
+			IsVariadic:    false,
+		},
+	}
+}
+
+// Register registers the function with the registry
+func (f *VersionFunction) Register(registry funcregistry.Registry) {
+	info := f.GetInfo()
+	registry.MustRegister(info)
+}
+
+// Evaluate returns the Stoolap version string
+func (f *VersionFunction) Evaluate(args ...interface{}) (interface{}, error) {
+	// Version string for Stoolap - can be updated as needed
+	return common.VersionString, nil
+}
+
+// NewVersionFunction creates a new VERSION function
+func NewVersionFunction() contract.ScalarFunction {
+	return &VersionFunction{}
+}
+
+// Self-registration
+func init() {
+	// Register the VERSION function with the global registry
+	if registry := registry.GetGlobal(); registry != nil {
+		registry.RegisterScalarFunction(NewVersionFunction())
+	}
+}
