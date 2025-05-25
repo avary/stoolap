1efcede9b8ae85f7f9a1dc4e5aeaa3a46cb2bdec
semihalev <semihalev@gmail.com>
2025-05-25 11:21:23 +0300

    Added go.work file

--- internal/common/version.go
+++ internal/common/version.go
@@ -23,7 +23,7 @@ const (
 	// VersionPatch is the patch version of the driver
 	VersionPatch = "6"
 	// VersionSuffix is the suffix of the driver version
-	VersionSuffix = "ec799ba2" // git commit hash
+	VersionSuffix = "628701df" // git commit hash
 
 	// VersionString is the version string of the driver
 	VersionString = "Stoolap v" + VersionMajor + "." + VersionMinor + "." + VersionPatch + "-" + VersionSuffix
